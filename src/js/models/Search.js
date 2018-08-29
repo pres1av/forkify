5a06af4ed887b04496d3a07272aef6701a7b6262
WHF-Chefo <chefopld@gmail.com>
2018-08-30 00:43:36 +0100

    build 29.08.2018

---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const apiKey = '6c03936f1278ed16e0633d06070fbf95';
+
+        try {
+            const res = await axios(`http://food2fork.com/api/search?key=${apiKey}&q=${this.query}`);
+            this.result = res.data.recipes;
+        } catch(error) {
+            alert(error);
+        }
+    }
+}
